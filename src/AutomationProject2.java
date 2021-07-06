afa4e32745162418215fdb25053ff9a2f4a11931
Cristian <cristian.fortuna01@gmail.com>
2021-07-06 16:12:35 -0400

    first commit

--- src/AutomationProject2.java
+++ src/AutomationProject2.java
@@ -16,22 +16,30 @@ import java.util.concurrent.TimeUnit;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 public class AutomationProject2 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
 //      1. Launch Chrome browser.
         System.setProperty("webdriver.chrome.driver","/Users/cristianfortuna/Documents/drivers/chromedriver ");
         WebDriver wd = new ChromeDriver();
         wd.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 //      2. Navigate to http://secure.smartbearsoftware.com/samples/TestComplete12/WebOrders/Login.aspx
         wd.navigate().to("http://secure.smartbearsoftware.com/samples/TestComplete12/WebOrders/Login.aspx");
+        Thread.sleep(2000);
+
 //      3. Login using username Tester and password test
         wd.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester", Keys.TAB, "test", Keys.ENTER);
+        Thread.sleep(2000);
+
 //      4. Click on Order link
         wd.findElement(By.linkText("Order")).click();
+        Thread.sleep(2000);
+
 //      5. Enter a random product quantity between 1 and 100
         String product = wd.findElement(By.name("ctl00$MainContent$fmwOrder$ddlProduct")).getAttribute("value");
         int randomQuantity = (int) (Math.random() * 99) + 1;
         wd.findElement(By.id("ctl00_MainContent_fmwOrder_txtQuantity"))
                 .sendKeys(Keys.BACK_SPACE, String.valueOf(randomQuantity), Keys.ENTER);
+        Thread.sleep(2000);
+
 /*      6. Click on Calculate and verify that the Total value is correct.
         Price per unit is 100.  The discount of 8 % is applied to quantities of 10+.
         So for example, if the quantity is 8, the Total should be 800.
@@ -48,6 +56,8 @@ public class AutomationProject2 {
             expectedTotalValue = 0;
         }
         assertTrue(actualTotalValue == expectedTotalValue);
+        Thread.sleep(2000);
+
 //      6.1 Generate and enter random first name and last name.
 //      7. Generate and Enter random street address
 //      8. Generate and Enter random city
@@ -70,6 +80,7 @@ public class AutomationProject2 {
         }
         wd.findElement(By.id("ctl00_MainContent_fmwOrder_txtName")).sendKeys(randomMockData[0]+" "+randomMockData[1],
                 Keys.TAB, randomMockData[2], Keys.TAB, randomMockData[3], Keys.TAB, randomMockData[4], Keys.TAB, randomMockData[5]);
+        Thread.sleep(2000);
 //
 //       11. Select the card type randomly. On each run your script should select a random type.
         String[] cardTypeId = {"ctl00_MainContent_fmwOrder_cardList_0",
@@ -81,6 +92,7 @@ public class AutomationProject2 {
             randomCardTypeId = cardTypeId[randomIndex];
         }
         wd.findElement(By.id(randomCardTypeId)).click();
+        Thread.sleep(2000);
 //
 //      12. Generate and enter the random card number:
 //        If Visa is selected, the card number should start with 4.
@@ -104,19 +116,24 @@ public class AutomationProject2 {
             wd.findElement(By.id("ctl00_MainContent_fmwOrder_TextBox6")).sendKeys(inputtedCardNumber);
         }
         String cardType = wd.findElement(By.id(randomCardTypeId)).getAttribute("value");
+        Thread.sleep(2000);
 //
 //      13. Enter a valid expiration date (newer than the current date)
         String expireDate = "12/22";
         wd.findElement(By.id("ctl00_MainContent_fmwOrder_TextBox1")).sendKeys(expireDate);
+        Thread.sleep(2000);
 //
 //      14. Click on Process
         wd.findElement(By.id("ctl00_MainContent_fmwOrder_InsertButton")).click();
+        Thread.sleep(2000);
 //
 //      15. Verify that "New order has been successfully added" message appeared on the page.
         assertTrue(wd.getPageSource().contains("New order has been successfully added."));
+        Thread.sleep(2000);
 //
 //      16. Click on View All Orders link.
         wd.findElement(By.linkText("View all orders")).click();
+        Thread.sleep(2000);
 //
 //      17. The placed order details appears on the first row of the orders table. Verify that the entire information
 //      contained on the row (Name, Product, Quantity, etc) matches the previously entered information in previous steps.
@@ -128,7 +145,8 @@ public class AutomationProject2 {
                 +todaysDate+" "+randomMockData[2]+" "+randomMockData[3]+" "+randomMockData[4]+" "
                 +randomMockData[5]+" "+cardType+" "+inputtedCardNumber+" "+expireDate;
         assertEquals(actualFirstRowOfTr, expectedFirstRowOfTr);
-//
+        Thread.sleep(2000);
+
 //      18. Log out of the application.
         wd.findElement(By.id("ctl00_logout")).click();
         wd.quit();
